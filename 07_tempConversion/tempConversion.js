87b47f593b3385513538b810a07a5e60a6ee04fa
Tomas Petrikas <tomas.petrikas99@gmail.com>
2021-12-27 10:59:23 +0200

    7th exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let result = +(((temperature - 32) * 5/9).toFixed(1));
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let result = +(((temperature * 9/5) + 32).toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
