21c140738fafb026b1af156812f27445c3fa7e97
Tomas Petrikas <tomas.petrikas99@gmail.com>
2021-12-27 10:24:23 +0200

    4th exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+  // This function expects an array and some elements to remove from the array
+  // If there's nothing to remove, return the array
+  if (arguments.length < 2) return array;
 
+  let elementPos;
+  for (let i = 1; i < arguments.length; i++) {
+    elementPos = array.indexOf(arguments[i]);
+    
+    if (elementPos !== -1) {
+      array.splice(elementPos, 1);
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
