9e420372855a0c96a6db3aa89d98b07380f71f60
Tomas Petrikas <tomas.petrikas99@gmail.com>
2021-12-27 10:02:27 +0200

    3rd exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
 const reverseString = function(str) {
+  // "Hello" => ["H", "e", "l", "l", "o"]
+  let splitString = str.split("");
 
+  // ["H", "e", "l", "l", "o"] => ["o", "l", "l", "e", "H"]
+  let reverseArray = splitString.reverse();
+
+  // ["o", "l", "l", "e", "H"] => "olleH"
+  let reverseString = reverseArray.join("");
+
+  return reverseString;
 };
 
 // Do not edit below this line
