a54d1c56493ca0fb8ea063f88d7f3faee7649137
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-01-03 12:10:45 +0200

    12th exercise done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldest = people.sort(function(a, b) {
+    if (!("yearOfDeath" in a)) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    if (!("yearOfDeath" in b)) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
 
-};
+    const firstPerson = a.yearOfDeath - a.yearOfBirth;
+    const secondPerson = b.yearOfDeath - b.yearOfBirth;
+    if (firstPerson > secondPerson) {
+      return -1;
+    }
+    else {
+      return 1;
+    }
+  });
+
+  return oldest[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
