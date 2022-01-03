40526f95f618bfa85fcb6c1025970b6cbf9b617d
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-01-03 11:32:13 +0200

    10th exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
 
-};
+  // Sum the series? Like a pleb? Who do you take me for?
+  // Binet's Fibonacci Number Formula
+  // https://mathworld.wolfram.com/BinetsFibonacciNumberFormula.html
+
+  const phi = (1 + Math.sqrt(5)) / 2;
+  const F_n = (phi**n - (-phi)**(-n)) / Math.sqrt(5);
+
+  return Math.round(F_n);
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
