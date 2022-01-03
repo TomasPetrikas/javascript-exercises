0f4ef1087195e58b57f1c30c51110d2043c34947
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-01-03 10:57:15 +0200

    9th exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // The regex strips non-alphanum characters from str
+  str = str.replace(/\W/g, '').toLowerCase();
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != str[str.length - 1 - i]) return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
