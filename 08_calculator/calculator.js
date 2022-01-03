8c5bceba52afdf102cf137aa5a8bb1e87ab6309d
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-01-03 10:25:29 +0200

    8th exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	const totalSum = numbers.reduce((total, number) => {
+    return total + number;
+  }, 0);
+  return totalSum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  if (numbers.length === 0) return 0;
 
+  const totalSum = numbers.reduce((total, number) => {
+    return total * number;
+  }, 1);
+  return totalSum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
